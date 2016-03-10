d409aa9ba9cd2eeb7b63008f2d4d1f46631f84c9
tyreekus <ty@tyhuffman.com>
2016-03-10 15:22:50 -0500

    2016-03-10 1521
    Replaced old script with new one that is functioning.

--- PasswordExpirationViaEmail.ps1
+++ PasswordExpirationViaEmail.ps1
@@ -1,96 +1,37 @@
-##############Variables#################            
-$verbose = $true            
-$notificationstartday = 14            
-$sendermailaddress = "no-reply@contoso.com"            
-$SMTPserver = "mail.contoso.com"            
-$DN = "DC=contoso,DC=local"            
-########################################            
-            
-##############Function##################            
-function PreparePasswordPolicyMail ($ComplexityEnabled,$MaxPasswordAge,$MinPasswordAge,$MinPasswordLength,$PasswordHistoryCount)            
-{            
-    $verbosemailBody = "Below is a summary of the applied Password Policy settings:`r`n`r`n"            
-    $verbosemailBody += "Complexity Enabled = " + $ComplexityEnabled + "`r`n`r`n"            
-    $verbosemailBody += "Maximum Password Age = " + $MaxPasswordAge + "`r`n`r`n"            
-    $verbosemailBody += "Minimum Password Age = " + $MinPasswordAge + "`r`n`r`n"            
-    $verbosemailBody += "Minimum Password Length = " + $MinPasswordLength + "`r`n`r`n"            
-    $verbosemailBody += "Remembered Password History = " + $PasswordHistoryCount + "`r`n`r`n"            
-    return $verbosemailBody            
-}            
-            
-function SendMail ($SMTPserver,$sendermailaddress,$usermailaddress,$mailBody)            
-{            
-    $smtpServer = $SMTPserver            
-    $msg = new-object Net.Mail.MailMessage            
-    $smtp = new-object Net.Mail.SmtpClient($smtpServer)            
-    $msg.From = $sendermailaddress            
-    $msg.To.Add($usermailaddress)            
-    $msg.Subject = "Your password is about to expire"            
-    $msg.Body = $mailBody            
-    $smtp.Send($msg)            
-}            
-########################################            
-            
-##############Main######################            
-$domainPolicy = Get-ADDefaultDomainPasswordPolicy            
-$passwordexpirydefaultdomainpolicy = $domainPolicy.MaxPasswordAge.Days -ne 0            
-            
-if($passwordexpirydefaultdomainpolicy)            
-{            
-    $defaultdomainpolicyMaxPasswordAge = $domainPolicy.MaxPasswordAge.Days            
-    if($verbose)            
-    {            
-        $defaultdomainpolicyverbosemailBody = PreparePasswordPolicyMail $PSOpolicy.ComplexityEnabled $PSOpolicy.MaxPasswordAge.Days $PSOpolicy.MinPasswordAge.Days $PSOpolicy.MinPasswordLength $PSOpolicy.PasswordHistoryCount            
-    }            
-}            
-            
-foreach ($user in (Get-ADUser -SearchBase $DN -Filter * -properties mail))            
-{            
-    $samaccountname = $user.samaccountname            
-    $PSO= Get-ADUserResultantPasswordPolicy -Identity $samaccountname            
-    if ($PSO -ne $null)            
-    {                         
-        $PSOpolicy = Get-ADUserResultantPasswordPolicy -Identity $samaccountname            
-        $PSOMaxPasswordAge = $PSOpolicy.MaxPasswordAge.days            
-        $pwdlastset = [datetime]::FromFileTime((Get-ADUser -LDAPFilter "(&(samaccountname=$samaccountname))" -properties pwdLastSet).pwdLastSet)            
-        $expirydate = ($pwdlastset).AddDays($PSOMaxPasswordAge)            
-        $delta = ($expirydate - (Get-Date)).Days            
-        $comparionresults = (($expirydate - (Get-Date)).Days -le $notificationstartday) -AND ($delta -ge 1)            
-        if ($comparionresults)            
-        {            
-            $mailBody = "Dear " + $user.GivenName + ",`r`n`r`n"            
-            $mailBody += "Your password will expire after " + $delta + " days. You will need to change your password to keep using it.`r`n`r`n"            
-            if ($verbose)            
-            {            
-                $mailBody += PreparePasswordPolicyMail $PSOpolicy.ComplexityEnabled $PSOpolicy.MaxPasswordAge.Days $PSOpolicy.MinPasswordAge.Days $PSOpolicy.MinPasswordLength $PSOpolicy.PasswordHistoryCount            
-            }            
-            $mailBody += "`r`n`r`nYour IT Department"            
-            $usermailaddress = $user.mail            
-            SendMail $SMTPserver $sendermailaddress $usermailaddress $mailBody            
-        }            
-    }            
-    else            
-    {            
-        if($passwordexpirydefaultdomainpolicy)            
-        {            
-            $pwdlastset = [datetime]::FromFileTime((Get-ADUser -LDAPFilter "(&(samaccountname=$samaccountname))" -properties pwdLastSet).pwdLastSet)            
-            $expirydate = ($pwdlastset).AddDays($defaultdomainpolicyMaxPasswordAge)            
-            $delta = ($expirydate - (Get-Date)).Days            
-            $comparionresults = (($expirydate - (Get-Date)).Days -le $notificationstartday) -AND ($delta -ge 1)            
-            if ($comparionresults)            
-            {            
-                $mailBody = "Dear " + $user.GivenName + ",`r`n`r`n"            
-                $delta = ($expirydate - (Get-Date)).Days            
-                $mailBody += "Your password will expire after " + $delta + " days. You will need to change your password to keep using it.`r`n`r`n"            
-                if ($verbose)            
-                {            
-                    $mailBody += $defaultdomainpolicyverbosemailBody            
-                }            
-                $mailBody += "`r`n`r`nYour IT Department"            
-                $usermailaddress = $user.mail            
-                SendMail $SMTPserver $sendermailaddress $usermailaddress $mailBody            
-            }            
-            
-        }            
-    }            
+$TargetDateBegin = (get-date)
+$TargetDateEnd = (Get-date).AddDays(14)
+$PSEmailServer = <MAILSERVER>
+
+###################################################################
+foreach ($user in (Get-ADGroupMember -Identity "OU OF USERS TO APPLY TO.  CAN CHANGE TO APPLY TO ENTIRE DOMAIN")) {
+    $UserEmailAddress = (get-aduser $user -properties mail).mail
+    $UserExpiraryDate = Get-ADUser $user.SamAccountName –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}
+    $EmailBody = $null
+    if (($UserExpiraryDate.ExpiryDate -ge $TargetDateBegin) -and ($UserExpiraryDate.ExpiryDate -le $TargetDateEnd)) {
+    $DaysTilExpire = ($UserExpiraryDate.ExpiryDate - $TargetDateBegin).Days
+$EmailBody = 
+@"
+Dear $($user.GivenName),
+
+Your password will expire after $DaysTilExpire days.  You will need to change your password to keep using your account.
+
+In order to change your password, you can follow the following steps:
+1. Click the Start button at the bottom-left portion of your screen and click Windows Security, which will be the 4th option from the top in the right column.
+                      -OR-
+   Pressing CTRL+ALT+END key combination on your keyboard.
+2. Click "Change a Password..."
+3. Type your old password in the second box, then type in a new password in the third and fourth box.
+4. Once finished, click the Blue Arrow button to finish changing your password.
+
+If you have any issues, please feel free to contact Secant Support and we can assist with the process. 
+
+Sincerely,
+Your IT Department
+
+"@
+    if ($EmailBody -ne $null) {
+        Send-MailMessage -from 'no-reply@contoso.com' -To $UserEmailAddress -Subject 'Password Expiration Notification' -Body $EmailBody -SmtpServer $PSEmailServer
+        }
+    }
+    
 }
