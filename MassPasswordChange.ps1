4531282df27f490a03e0580a7ce37691085522fc
tyreekus <ty@tyhuffman.com>
2016-04-11 09:49:28 -0400

    Update MassPasswordChange.ps1

--- MassPasswordChange.ps1
+++ MassPasswordChange.ps1
@@ -1,7 +1,8 @@
 $SearchBase = <INSERTOUPATH>
+$NewPassword = read-host "Enter the new password to set: "
 #
 #
 foreach ($i in get-aduser -filter * -searchbase $SearchBase) {
-    Set-ADAccountPassword -NewPassword (ConvertTo-SecureString -String "rl1018" -AsPlainText -force) -Identity $i
+    Set-ADAccountPassword -NewPassword (ConvertTo-SecureString -String $NewPassword -AsPlainText -force) -Identity $i
     Enable-ADAccount -Identity $i
     }
