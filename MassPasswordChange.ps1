6454f6e58a525ed79225670b532755973e8fea2d
tyreekus <ty@tyhuffman.com>
2016-06-08 14:21:40 -0400

    6-8-2016 1421
    Missed a bracket

--- MassPasswordChange.ps1
+++ MassPasswordChange.ps1
@@ -6,7 +6,7 @@ $ChangeAtLogon = Read-Host "Force change password at next logon (y/n): "
 foreach ($i in get-aduser -filter * -searchbase $SearchBase) {
     Set-ADAccountPassword -NewPassword (ConvertTo-SecureString -String $NewPassword -AsPlainText -force) -Identity $i
     Enable-ADAccount -Identity $i
-    if ($ChangeAtLogon -eq 'y' {
+    if ($ChangeAtLogon -eq 'y') {
         set-aduser -Identity $i -ChangePasswordAtLogon $true
         }
     }
