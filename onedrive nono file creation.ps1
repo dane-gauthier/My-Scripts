2c29c622edbfec2601392a1b7272a34c0ea56bb3
tyreekus <ty@tyhuffman.com>
2016-08-11 14:19:21 -0400

    Added creating Folders with double space

--- onedrive nono file creation.ps1
+++ onedrive nono file creation.ps1
@@ -8,3 +8,6 @@ new-item -Path 'C:\test\Test&3.txt' -ItemType File
 new-item -path 'C:\test\test  1.txt' -ItemType File
 new-item -path 'C:\test\test  2.txt' -ItemType File
 new-item -path 'C:\test\test  3.txt' -ItemType File
+New-Item -path 'C:\test\test  1' -ItemType Directory
+New-Item -path 'C:\test\test  2' -ItemType Directory
+New-Item -path 'C:\test\test  3' -ItemType Directory
