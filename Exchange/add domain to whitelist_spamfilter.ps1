1e574d1431917afe5890a218d8e5f1edc250cdf8
tyreekus <ty@tyhuffman.com>
2016-08-31 11:15:26 -0400

    commit message

--- Exchange/add domain to whitelist_spamfilter.ps1
+++ Exchange/add domain to whitelist_spamfilter.ps1
@@ -1,5 +1,5 @@
 $NewDomain = read-host "new domain to whitelist"
 
 $list = (Get-ContentFilterConfig).BypassedSenderDomains
-$list.add(“$NewDomain”)
+$list.add("$NewDomain")
 Set-ContentFilterConfig -BypassedSenderDomains $list
