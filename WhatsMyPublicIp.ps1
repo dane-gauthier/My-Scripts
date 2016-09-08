64ba8621745cb29d91f108e514b1c7bae34ad5a3
tyreekus <ty@tyhuffman.com>
2016-09-08 09:06:19 -0400

    Added list of things to check for

--- WhatsMyPublicIp.ps1
+++ WhatsMyPublicIp.ps1
@@ -1,3 +1,10 @@
+<#
+Things to check for:
+1. Network interface address
+2. Ping to 8.8.8.8,8.8.4.4,4.2.2.1,4.2.2.2
+3. Ping google.com
+#>
+
 $ip = Invoke-WebRequest -URI "http://icanhazip.com"
 write-output "Your Public IP address is: $ip"
 pause
