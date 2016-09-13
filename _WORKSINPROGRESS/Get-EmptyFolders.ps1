5b0a971bb202f3700371857736e6f7d40d10ef4b
tyreekus <ty@tyhuffman.com>
2016-09-12 20:28:33 -0400

    Added WIP folder

--- _WORKSINPROGRESS/Get-EmptyFolders.ps1
+++ _WORKSINPROGRESS/Get-EmptyFolders.ps1
@@ -1,7 +1,7 @@
 $BaseDir = read-host "Base Directory to scan"
 
-$a = gci $BaseDir -r | ? {$_.PSIsContainer -eq $True} | ? {$_.GetFiles().Count -eq 0}
-Write-host $a.FullName
+$a = gci $BaseDir -r | ? {$_.PSIsContainer -eq $True} | ? {$_.GetFiles().Count -eq 0} | select FullName
+Write-Output $a.FullName
 
 $Removal = read-host "Do you want to remove the empty folders? (Press y to continue)"
 
