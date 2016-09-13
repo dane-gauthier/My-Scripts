6a2b36c59508d79958bae3ac18f58873818df241
tyreekus <ty@tyhuffman.com>
2016-09-12 20:02:23 -0400

    Added script to scan/remove empty folders

---
+++ Windows/Get-EmptyFolders.ps1
@@ -0,0 +1,10 @@
+$BaseDir = read-host "Base Directory to scan"
+
+$a = gci $BaseDir -r | ? {$_.PSIsContainer -eq $True} | ? {$_.GetFiles().Count -eq 0}
+Write-host $a.FullName
+
+$Removal = read-host "Do you want to remove the empty folders? (Press y to continue)"
+
+if ($Removal -eq "y") {
+	$a | remove-item
+	}
