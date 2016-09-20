29a97c1cca3052a2981f0098ee71b3be9ef0e851
tyreekus <ty@tyhuffman.com>
2016-09-20 08:01:47 -0400

    put in tracert switch and added root dns server resolution

--- NetworkInfo.ps1
+++ NetworkInfo.ps1
@@ -5,6 +5,12 @@ Things to check for:
 3. Ping google.com and other websites
 #>
 
+
+# Whether or not to include a traceroute
+Param(
+[switch]$IncludeTrace
+)
+
 $nwINFO = Get-WmiObject -ComputerName $ENV:computername Win32_NetworkAdapterConfiguration | Where-Object { $_.IPAddress -ne $null -AND $_.Description -ne "VirtualBox Host-Only Ethernet Adapter" }
 
 $nwServerName = $nwINFO.DNSHostName
@@ -42,11 +48,28 @@ Test-Connection -ComputerName google.com -count 1
 Test-Connection -ComputerName facebook.com -count 1
 Test-Connection -ComputerName yahoo.com -count 1
 Test-Connection -ComputerName twitter.com -count 1
-echo " "
-#tracert to google's public address
-tracert google.com
+#Test connection to root DNS servers
+write-host "Now resolving and testing connection to root DNS servers"
+test-connection -ComputerName a.root-servers.net -count 1
+test-connection -ComputerName b.root-servers.net -count 1
+test-connection -ComputerName c.root-servers.net -count 1
+test-connection -ComputerName d.root-servers.net -count 1
+test-connection -ComputerName e.root-servers.net -count 1
+test-connection -ComputerName f.root-servers.net -count 1
+test-connection -ComputerName g.root-servers.net -count 1
+test-connection -ComputerName h.root-servers.net -count 1
+test-connection -ComputerName i.root-servers.net -count 1
+test-connection -ComputerName j.root-servers.net -count 1
+test-connection -ComputerName k.root-servers.net -count 1
+test-connection -ComputerName l.root-servers.net -count 1
+test-connection -ComputerName m.root-servers.net -count 1
 echo " "
 
+if ($IncludeTrace -eq $True) {
+    #tracert to google's public address
+    tracert google.com
+    echo " "
+}
 
 pause
 #endregion
