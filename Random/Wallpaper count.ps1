404f839b219bbef7994c87246df88e98835e435a
tyreekus <ty@tyhuffman.com>
2016-09-25 14:56:45 -0400

    added more file extensions

--- Random/Wallpaper count.ps1
+++ Random/Wallpaper count.ps1
@@ -2,5 +2,5 @@ Param(
 	[string]$Path
 	)
 
-$WallpaperCount = (gci -r | ? {$_.PSIsContainer -eq $false -and ($_.Extension -match '.jpg' -or $_.extension -match '.jpeg' -or $_.extension -match '.png' -or $_.extension -match '.bmp')}).count
+$WallpaperCount = (gci -r | ? {$_.PSIsContainer -eq $false -and ($_.Extension -match '.jpg' -or $_.extension -match '.jpeg' -or $_.extension -match '.png' -or $_.extension -match '.bmp' -or $_.extension -match '.jpeg')}).count
 write-host "Congratulations, you have $WallpaperCount wallpapers!"
