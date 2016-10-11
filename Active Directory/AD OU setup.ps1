8de1fcd5559cd04631c376bcdea646d5398c8690
tyreekus <ty@tyhuffman.com>
2016-10-11 12:25:07 -0400

    made changes

--- Active Directory/AD OU setup.ps1
+++ Active Directory/AD OU setup.ps1
@@ -9,4 +9,12 @@ New-ADObject -type OrganizationalUnit -name 'Users' -path "OU=$domainName,dc=$do
 New-ADObject -type OrganizationalUnit -name 'Security Groups' -path "OU=$domainName,dc=$domainName,dc=$domainSuffix"
 New-ADObject -type OrganizationalUnit -name 'Member Servers' -path "OU=$domainName,dc=$domainName,dc=$domainSuffix"
 New-ADObject -type OrganizationalUnit -name 'Admin Accounts' -path "OU=$domainName,dc=$domainName,dc=$domainSuffix"
+New-ADObject -type OrganizationalUnit -name 'Service Accounts' -path "OU=$domainName,dc=$domainName,dc=$domainSuffix"
 New-ADObject -type OrganizationalUnit -name 'Distribution Groups' -path "OU=$domainName,dc=$domainName,dc=$domainSuffix"
+
+<#Security Groups
+New-ADgroup -GroupCategory security -GroupScope Global -Name $name -Path "OU=Security Groups,OU=$domainName,DC=$domainName,DC=$domainSuffix"
+#>
+
+#Users Folder Groups
+New-ADObject -type OrganizationalUnit -name 'Disabled Users' -Path "OU=$domainName,dc=$domainName,dc=$domainSuffix"
