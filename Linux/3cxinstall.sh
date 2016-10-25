a3487b34577f5c3dc45e49fdb4aa26d166246c4e
tyreekus <ty@tyhuffman.com>
2016-10-25 09:21:32 -0400

    added commands to install 3cx in debian

---
+++ Linux/3cxinstall.sh
@@ -0,0 +1,6 @@
+wget -O- http://downloads.3cx.com/downloads/3cxpbx/public.key | apt-key add -
+echo "deb http://downloads.3cx.com/downloads/3cxpbx/ /" | tee /etc/apt/sources.list.d/3cxpbx.list
+
+apt-get update
+
+apt-get install 3cxpbx
