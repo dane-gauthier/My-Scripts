8987c126e9b1167676b90729b17ce44fbfb66d13
tyreekus <ty@tyhuffman.com>
2016-11-29 23:07:51 -0500

    removed old files
    Signed-off-by: tyreekus <ty@tyhuffman.com>

--- _WORKSINPROGRESS/download pictures from site.ps1
+++
@@ -1,6 +0,0 @@
-$website = read-host "Url of site images to download"
-
-foreach ($i in ((wget $website).images).src) {
-    $randomNumber = Get-Random
-    Invoke-WebRequest $i -OutFile "$randomNumber.jpg"
-}
