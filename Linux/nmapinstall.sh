5d4a2c351a2b12fa17fd102915f1470cb24c1860
tyreekus <ty@tyhuffman.com>
2017-02-07 12:14:17 -0500

    removed bad script

--- Linux/nmapinstall.sh
+++
@@ -1,6 +0,0 @@
-
-
-rpm -vhU https://nmap.org/dist/nmap-7.40-1.x86_64.rpm
-rpm -vhU https://nmap.org/dist/zenmap-7.40-1.noarch.rpm
-rpm -vhU https://nmap.org/dist/ncat-7.40-1.x86_64.rpm
-rpm -vhU https://nmap.org/dist/nping-0.7.40-1.x86_64.rpm
