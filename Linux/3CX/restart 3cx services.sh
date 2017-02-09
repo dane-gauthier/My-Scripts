be836a1228f5eeb639986b816a9ab67430a05a90
tyreekus <ty@tyhuffman.com>
2017-02-09 15:25:05 -0500

    fixed script

--- Linux/3CX/restart 3cx services.sh
+++ Linux/3CX/restart 3cx services.sh
@@ -1,9 +1,2 @@
-systemctl restart 3CXCfgServ01.service
-systemctl restart 3CXEventNotificationManager.service
-systemctl restart 3CXIVR01.service
-systemctl restart 3CXMediaServer.service
-systemctl restart 3CXPhoneSystem01.service
-systemctl restart 3CXPhoneSystemMC01.service
-systemctl restart 3CXQueueManager01.service
-systemctl restart 3CXSystemService01.service
-systemctl restart 3CXTunnel01.service
+/usr/sbin/3CXStopServices
+/usr/sbin/3CXStartServices
