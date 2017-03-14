6f461985baf2caac156e3fac744436cfc057bdd4
tyreekus <ty@tyhuffman.com>
2017-03-14 11:23:07 -0400

    -removed description variable from template -changed from static variable to changable variable -added option to use default password 'Password1'

--- Active Directory/MassPasswordChange.ps1
+++ Active Directory/MassPasswordChange.ps1
@@ -1,4 +1,4 @@
-$SearchBase = <PATHTOSEARCHBASE>
+$SearchBase = read-host 'OU path to users'
 $NewPassword = read-host "Enter the new password to set"
 $ChangeAtLogon = Read-Host "Force change password at next logon (y/n)"
 #
