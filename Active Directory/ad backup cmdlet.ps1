9f5d9640f1b2c07e5265ca1a7a945671ce15398a
tyreekus <ty@tyhuffman.com>
2017-03-14 08:26:39 -0400

    changed from iex to invoke-expression

--- Active Directory/ad backup cmdlet.ps1
+++ Active Directory/ad backup cmdlet.ps1
@@ -1,1 +1,1 @@
-iex (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/tyreekus/Script-share/master/Scripts/AD/ad%20backup.ps1")
+Invoke-Expression (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/tyreekus/Script-share/master/Scripts/AD/ad%20backup.ps1")
