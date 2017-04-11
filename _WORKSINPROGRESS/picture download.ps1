929bf15f766a62f0f864f019db25f80127b701a1
tyreekus <ty@tyhuffman.com>
2017-04-11 16:57:06 -0400

    moved file to Random

--- _WORKSINPROGRESS/picture download.ps1
+++
@@ -1,9 +0,0 @@
-$URL = read-host 'url to search'
-
-$list = ((iwr -uri $URL).images).src
-
-foreach ($i in ($list -replace '//','')) {
-    $filename = $i -replace 'i.imgur.com/',''
-    iwr -uri $i -OutFile $filename
-}
-
