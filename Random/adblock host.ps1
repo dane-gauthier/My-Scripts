e36d41a6fbbc9a38b1a13d1a8cba96976264ed08
tyreekus <ty@tyhuffman.com>
2017-04-19 08:49:39 -0400

    changed wget to invoke-webrequest

--- Random/adblock host.ps1
+++ Random/adblock host.ps1
@@ -1,2 +1,2 @@
 #Requires -RunAsAdministrator
-(wget http://winhelp2002.mvps.org/hosts.txt).Content > C:\windows\system32\drivers\etc\HOSTS
+(Invoke-WebRequest -Uri http://winhelp2002.mvps.org/hosts.txt).Content > C:\windows\system32\drivers\etc\HOSTS
