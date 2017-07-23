b847d1b87ecd247fa7501e37735e3867c285fdd4
Your Name <your@email.com>
2017-07-22 20:42:48 -0400

    removed vboxmanage script

--- _WORKSINPROGRESS/Virtualbox VM Setup.ps1
+++
@@ -1,9 +0,0 @@
-#---Variables---
-$VBoxManage = 'C:\Program Files\Oracle\VirtualBox\VBoxManage.exe'
-$VMName = read-host 'Name of new Virtual Machine'
-$VMType = read-host 'Operating System Type'
-
-#region Main Script
-& $VBoxManage "createvm --name $VMName"
-
-#endregion
