d43072a19113d23fae065392e03ea1b25df91781
Ty Huffman <Ty.Huffman@alpha-capitalmarkets.local>
2017-07-25 09:24:16 -0400

    added variable for entering domain to join

--- Windows/JoinComputerToDomain.ps1
+++ Windows/JoinComputerToDomain.ps1
@@ -1,3 +1,5 @@
+$DomainName = Read-Host 'Enter the domain you are joining'
+
 $cred = New-Object System.Management.Automation.PsCredential("domain\useraccountwithjoinpermissions", (ConvertTo-SecureString "useraccountpassword" -AsPlainText -Force))
-Add-Computer -DomainName "your.domain.here" -Credential $cred
+Add-Computer -DomainName $DomainName -Credential $cred
 Restart-Computer
